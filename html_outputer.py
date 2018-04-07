6ca58652bb4c353ccf336bc71590b03ddc187f76
马海琴 <mahq@servyou.com.cn>
2018-04-07 15:34:28 +0800

    修改部分样式

--- html_outputer.py
+++ html_outputer.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python2
 # -*- coding: UTF-8 -*-
 class HtmlOutputer(object):
-
     def __init__(self):
         self.datas = []
-
 
     def collect_data(self, data):
         if data is None:
@@ -15,12 +13,18 @@ class HtmlOutputer(object):
         fout = open('output.html', 'w')
         fout.write("<html>")
         fout.write("<body>")
-        fout.write("<table>")
-
+        fout.write(
+            "<table border=\"1\" width=\"100%\" table-layout=\"fixed\" cellpadding=\"0\" cellspacing=\"0\" style=\"word-break:  break-all;\">")
+        fout.write("<caption><b>抓取到的词条内容</b></caption>")
+        fout.write(
+            "<tr style=\"color:green;\"><th width=\"10%\">URL</th><th width=\"10%\"> title </th ><th width=\"80%\"> summary </th ></tr >")
         for data in self.datas:
             fout.write("<tr>")
-            fout.write("<td>%s</td>" % data['url'])
-            fout.write("<td>%s</td>" % data['title'].encode('utf-8'))
+            fout.write(
+                "<td><a href=\" %s \">%s</a></td>" %
+                (data['url'], data['url']))
+            fout.write(
+                "<td>%s</td>" % data['title'].encode('utf-8'))
             fout.write("<td>%s</td>" % data['summary'].encode('utf-8'))
             fout.write("</tr>")
         fout.write("</table>")
--- spider_main.py
+++ spider_main.py
@@ -34,20 +34,17 @@ import url_manager, html_downloader, html_outputer, html_parser
 
 
 class SpiderMain(object):
-
     def __init__(self):
         self.urls = url_manager.UrlManager()
         self.downloader = html_downloader.HtmlDownloader()
         self.parser = html_parser.HtmlParser()
         self.outputer = html_outputer.HtmlOutputer()
 
-
-
     def craw(self, root_url):
         count = 1
         self.urls.add_new_url(root_url)
         while self.urls.has_new_url():
-            try :
+            try:
                 new_url = self.urls.get_new_url()
                 print 'craw %d : %s' % (count, new_url)
                 html_cont = self.downloader.download(new_url)
@@ -55,9 +52,7 @@ class SpiderMain(object):
                 self.urls.add_new_urls(new_urls)
                 self.outputer.collect_data(new_data)
 
-
-
-                if count == 1000:
+                if count == 10:
                     break
                 count = count + 1
             except:
@@ -65,8 +60,10 @@ class SpiderMain(object):
 
         self.outputer.output_html()
 
+
 if __name__ == "__main__":
-    root_url = "http://baike.baidu.com/view/21087.htm"
+    # root_url = "http://baike.baidu.com/view/21087.htm"
+    root_url = "https://baike.baidu.com/item/Python"
     obj_spider = SpiderMain()
     obj_spider.craw(root_url)
     print 'end'
